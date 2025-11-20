3bd4febe0773b6545fec8e89898f58a174e9ac41
HaBaisie <lawalbaisie1@gmail.com>
2025-11-20 13:08:31 +0100

    bbbb

--- hospitalmanagement/urls.py
+++ hospitalmanagement/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from hospital import views
+from django.conf import settings          # ← THIS LINE WAS MISSING
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
